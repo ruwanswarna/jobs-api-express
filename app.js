7b2a38afd88ec305fa342a0e8930f1be96f9ba3c
ruwanswarna <ruwanswarna@gmail.com>
2026-01-14 16:35:25 +0530

    change exports to common js require to module import

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+import "dotenv/config";
+import "express-async-errors";
+import express from "express";
 const app = express();
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
+app.get("/", (req, res) => {
+	res.send("jobs api");
 });
 
 app.use(notFoundMiddleware);
@@ -21,13 +21,13 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,10 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+import CustomAPIError from "../errors/custom-api.js";
+import { StatusCodes } from "http-status-codes";
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+	if (err instanceof CustomAPIError) {
+		return res.status(err.statusCode).json({ msg: err.message });
+	}
+	return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+};
 
-module.exports = errorHandlerMiddleware
+export default errorHandlerMiddleware;
